96337f620d1ff4585d5cf990f33cc30dc4a78d1b
DaeJin Jeong <devwithu@naver.com>
2017-10-08 13:55:34 +0900

    Page 356 10.5 메인화면구성

--- app/Http/Controllers/ProjectTaskController.php
+++ app/Http/Controllers/ProjectTaskController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Project;
+use App\Task;
 
 class ProjectTaskController extends Controller
 {
@@ -13,16 +15,16 @@ class ProjectTaskController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        $proj = Project::findOrFail($project);
+     public function index($project)
+     {
+         $proj = Project::findOrFail($project);
 
-        $tasks = $proj->tasks()->get();
+         $tasks = $proj->tasks()->get();
 
-        return view('project.task.index')
-                ->with('tasks', $tasks)
-                ->with('proj', $proj);
-    }
+         return view('project.task.index')
+                 ->with('tasks', $tasks)
+                 ->with('proj', $proj);
+     }
 
     /**
      * Show the form for creating a new resource.
@@ -62,10 +64,21 @@ class ProjectTaskController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
-    {
-        //
-    }
+     public function edit($projId, $taskId)
+     {
+         $user = \Auth::user();
+         $projects = $user->projects()->get();
+
+         $task = Task::findOrFail($taskId);
+
+         if($task->project->id != $projId) {
+             abort(403, '잘못된 접근입니다.');
+         }
+
+         return view('project.task.edit')
+             ->with('projects', $projects)
+             ->with('task', $task);
+     }
 
     /**
      * Update the specified resource in storage.
@@ -74,10 +87,30 @@ class ProjectTaskController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+     public function update(Request $request, $projId, $taskId)
+     {
+         $task = Task::findOrFail($taskId);
+
+         $project_id = $request->get('project_id');
+
+         // project id를 변경할 경우
+         if ($project_id != $task->project->id) {
+             // 변경된 project 검색
+             $project = Project::findOrFail($project_id);
+             $task->project()->associate($project);
+         }
+
+         $task->update([        // 2
+             'name' => $request->get('name'),
+             'description' => $request->get('description'),
+             'priority' => $request->get('priority'),
+             'status' => $request->get('status'),
+             'due_date' => $request->get('due_date'),
+         ]);
+
+         return redirect(route('project.task.index', $task->project->id))
+             ->with('message', $task->name . '가 수정되었습니다.');
+     }
 
     /**
      * Remove the specified resource from storage.
--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Project;
+use App\Task;
+use App\User;
 
 class WelcomeController extends Controller
 {
@@ -15,10 +18,9 @@ class WelcomeController extends Controller
     */
     public function index()  //2
     {
-        // 3 사용자, 프로젝트, 태스크 수 가져오기. 아직 모델을 생성하지 않았으므로 0으로 설정
-        $uc = 0; //User::count();
-        $pc = 0; //Project::count();
-        $tc = 0; //Task::count();
+        $uc = User::count();
+        $pc = Project::count();
+        $tc = Task::count();
 
         $total = [ 'user' => $uc,
             'project' => $pc,
